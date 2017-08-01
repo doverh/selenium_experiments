f3341811c37a9db0a03622be300ea133e5253c35
doverh <doverheld@gmail.com>
2017-08-01 13:47:22 -0400

    adjusts on download example

--- First_Selenium_Test/src/test/java/com/test/selenium/DownloadFile.java
+++ First_Selenium_Test/src/test/java/com/test/selenium/DownloadFile.java
@@ -2,10 +2,13 @@ package com.test.selenium;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
-
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 import java.io.File;
 import java.util.UUID;
 
@@ -23,7 +26,29 @@ public class DownloadFile {
         profile.setPreference("browser.download.folderList",2);
         profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "image/jpeg application/pdf, application/octetstream");
         profile.setPreference("pdfjs.disable",true);
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\dovico\\Documents\\jars\\geckodriver.exe");
         driver = new FirefoxDriver(profile);
+    }
+
+    @Test
+    public void download(){
+        driver.get("http://the-internet.herokuapp.com/download");
+        driver.findElement(By.cssSelector(".example a")).click();
+
+        //Wait 3 seconds to download the file
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        File[] listOfFiles= folder.listFiles();
+
+        //Make sure the directory is not empty
+        assertThat(listOfFiles.length, is(not(0)));
+        for(File file : folder.listFiles()){
+            assertThat(file.length(), is(not((long)0)));
+        }
+
     }
     @After
     public void tearDown() throws Exception{
